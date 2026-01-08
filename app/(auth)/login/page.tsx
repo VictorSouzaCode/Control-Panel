1ca5e474fda442ba92dc8640a964744860639378
creativeplot <victor.gamer1@hotmail.com>
2026-01-07 22:28:05 -0300

    login-layout created

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,32 @@
+'use client'
 // i Will use dummyJSON to fetch that data for this dashboard
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 const LoginPage = () => {
   return (
-    <div className="flex items-center justify-center">
-        <h1 className="text-6xl">Login</h1>
+    <div className="flex flex-col min-h-screen items-center justify-center gap-8">
+
+        <h1 className="text-6xl font-medium">Login</h1>
+
+        <div className="flex flex-col gap-4 w-full max-w-87.5">
+        <Input type="email" placeholder="Email"/>
+        <Input type="password" placeholder="Password"/>
+        </div>
+
+        <div className="flex gap-4 w-full max-w-87.5 justify-center">
+          
+          <Link href="/dashboard">
+          <Button size="lg">
+            Enter
+          </Button>
+          </Link>
+
+          <Button size="lg">
+            Login
+          </Button>
+        </div>
     </div>
   )
 }
