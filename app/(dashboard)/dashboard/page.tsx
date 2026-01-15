e8cdbd0d58619299fedeead2873c71a79ab4ec18
creativeplot <victor.gamer1@hotmail.com>
2026-01-15 12:15:13 -0300

    KpiCard created and being used in the overview page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,18 @@
+import KpiCards from "@/components/dashboard/KpiCards"
 
 const OverviewPage = () => {
   return (
-    <div className="flex flex-1 min-h-full">
-        <h1>OverviewPage</h1>
+    <div className="flex flex-col flex-1 min-h-full border">
+      <div className="flex flex-1 justify-around">
+        <KpiCards title="Total Customers" value="ronaldo"/>
+        <KpiCards title="Total Customers" value="ronaldo"/>
+        <KpiCards title="Total Customers" value="ronaldo"/>
+        <KpiCards title="Total Customers" value="ronaldo"/>
+      </div>
+
+      <div>
+        <h1>Another shit</h1>
+      </div>
     </div>
   )
 }
---
+++ components/dashboard/KpiCards.tsx
@@ -0,0 +1,24 @@
+import { Card, CardHeader, CardContent, CardTitle } from "../ui/card"
+
+type CardsProps = {
+    title: string,
+    value: number | string
+}
+
+const KpiCards = ({ title, value }: CardsProps) => {
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    {title}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <p>{value}</p>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default KpiCards
